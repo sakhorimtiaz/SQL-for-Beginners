e1f3508f72a07755724bc63f1f27d4527c6341ee
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2026-07-03 00:11:49 +0600

    Modify employee records: update and delete entries

---
+++ data_modification.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO employee(id, gender)
+VALUES(100, 'M'),(101, 'F');
+
+UPDATE employee where id=100
+set name='Maruf';
+
+
+DELETE FROM employee WHERE id=100;
+SELECT * FROM employee ORDER BY id DESC
+
