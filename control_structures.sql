832c31af13f6ef48cab4a590df0ecb91ee84e27d
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2026-07-03 10:49:03 +0600

    Add sales KPI checks and loop in control_structures.sql

--- control_structures.sql
+++ control_structures.sql
@@ -11,3 +11,26 @@ BEGIN
 	raise notice 'Sales exceeds KPT: %', sales;
 	END IF;
 END $$
+
+DO $$
+DECLARE
+	sales double precision:=0.00;
+	KPI double precision:=322966.749;
+BEGIN
+	select sum(total) into sales from supermarket_sales;
+	CASE
+	WHEN sales < KPI THEN
+	raise notice 'Sales are below KPT: %', sales;
+	WHEN sales = KPI THEN
+	raise notice 'Sales reached the exact KPT: %', sales;
+	ELSE 
+	raise notice 'Sales exceeds KPT: %', sales;
+	END CASE;
+END $$
+
+DO $$
+BEGIN
+	for counter in 1..10 loop
+		raise notice 'loop_counter: %',counter;
+	end loop;
+end $$
