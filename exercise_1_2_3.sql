30d761b5815ee97ed724a2bf694a7465d9bba708
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2026-07-03 14:20:23 +0600

    Add SQL queries for sales and landslide records

--- exercise_1_2_3.sql
+++ exercise_1_2_3.sql
@@ -11,3 +11,13 @@ CASE
 FROM supermarket_sales WHERE salesmonth=3
 and rating<=5
 and productline in ('Home and lifestyle','Food and beverages');
+
+SELECT row_id||country_code||to_char(hazard_date,'ddmmyyyy')||upper(left(landslide_size,1)) as book_id
+from landslide_records
+where trigger_cause='Tropical cyclone';
+
+select sum(total_amt), cat
+from retail_transaction T
+INNER JOIN retail_customer C ON T.customer_id=C.customer_id
+INNER JOIN retail_products P ON T.cat_code=P.cat_code
+group by cat order by sum(total_amt) desc
